5726d5d168d0ec8412908a3fc2a3f99a6b394c8a
Antreo Pukay <acrosswalker@yahoo.com>
2018-06-18 16:48:54 -0700

    app-info and us scaffolded

--- src/components/AppInfo.vue
+++ src/components/AppInfo.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <h2>App Info Page</h2>
+    <container id="app-info">
+    <div class="app-info">
+        <h3>About: Alchemy +Ultra</h3>
+        <p>Description coming soon!</p>
+    </div>
+</container>
   </div>
 </template>
 
@@ -11,5 +17,26 @@ export default {
 </script>
 
 <style>
+#app-info {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border-color: black;
+    border-radius: 40px;
+}
+.app-info {
+    font-family: 'Press Start 2P', cursive;
+    font-size: 18pt;
+    padding: 10px;
+    padding-top: 30px;
+    padding-bottom: 20px;
+    padding-left: 50px;
+    padding-right: 50px;
+    color: black;
+    margin: 20px;
+    box-shadow: 10px 10px 35px 2px;
+    border-radius: 35px;
+    background-color: steelblue;    
+}
 
 </style>
