b6ac15ef1b9ae225692eb2a363d2f9fdfc4f065e
Antreo Pukay <acrosswalker@yahoo.com>
2018-06-18 17:35:13 -0700

    home setup with image link, rough form input

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
     <h1>Daily Home Page</h1>
+    <section>
+      <p>Dont' take things too seriously!</P>
+      <img src="https://i.redditmedia.com/0ygjKXO3mq9oNEYwp5Prkdamaf55VCXIkk_77qRPHb0.jpg?s=befa078a11bea66a05f619cd60dbcdaa">
+    </section>
+
+    <section>
+      <div>
+        <form id="entry-form">
+          <h2>Create An Account</h2>
+
+        <input type="text" id="userName">
+        </form>
+      </div>
+    </section>
   </div>
 </template>
 
