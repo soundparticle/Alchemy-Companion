8250759250b56a5b4e5d2ec2949584428f6df7b0
Antreo Pukay <acrosswalker@yahoo.com>
2018-06-20 15:08:49 -0700

    delete functionality setup in Workspaces

--- src/components/Resources.vue
+++ src/components/Resources.vue
@@ -7,7 +7,10 @@
           :key="item.id"
           >
           <hr>
-          <h3> ( {{ item.upvotes }} ) </h3> &nbsp; Link from {{ item.first_name }}: &nbsp; <strong> {{ item.title }} &nbsp; - </strong> &nbsp; <a :href="item.url"> {{ item.description }} </a>
+          <h3> ( {{ item.upvotes }} ) </h3>
+          &nbsp; Link from {{ item.first_name }}:
+          &nbsp; <strong> {{ item.title }}&nbsp; - </strong>
+          &nbsp; <a :href="item.url"> {{ item.description }} </a>
         </li>
         <hr>
       </ul>
--- src/components/Workspaces.vue
+++ src/components/Workspaces.vue
@@ -2,13 +2,22 @@
   <div>
     <h1>Workspace Resources Page</h1>
   <pre v-if="error">{{ error }}</pre>
-    <ul v-if="workspaces">
+    <ul v-if="location">
       <li v-for="location in workspaces"
         :key="location.id"
         :onRemove="handleRemove"
         >
+        <button
+          :class="{ upvoted: votedPost }"
+          @click="handleVote">⬆️</button>
         <hr>
-        <h3> ( {{ location.upvotes }} ) </h3> &nbsp; Tip from {{ location.first_name }}: &nbsp; <strong>{{ location.title }} </strong> &nbsp; is a {{ location.workspace_type }}
+        <h3> ( {{ location.upvotes }} ) </h3>
+        <button>⭐</button>
+        <button v-if="user.id === location.authorID" @click="onRemove(location.id)">❌</button>
+        <button v-if="user.id === location.authorID">✏️</button>
+        &nbsp; location from {{ location.first_name }}:
+        &nbsp; <strong>{{ location.title }} </strong>
+        &nbsp; is a {{ location.workspace_type }}
         <p></p>
         Located at {{ location.address }} : &nbsp; <em> {{location.description}} </em>
       </li>
@@ -18,12 +27,13 @@
 </template>
 
 <script>
-import { getWorkspaces, removeWorkspace } from '../services/api';
+import { getWorkspaces, removeLocation } from '../services/api';
 
 export default {
   data() {
     return {
       workspaces: null,
+      location: null,
       error: null
     };
   },
@@ -37,17 +47,23 @@ export default {
         this.error = err;
       });
   },
-
-  handleRemove(id) {
-    if(confirm('Are you sure you want to delete?')) {
-      return removeWorkspace(id)
-        .then(()=> {
-          const index = this.workspace.findIndex(tip => tip.id === id);
-          if(index === -1) return;
-          this.workspace.splice(index, 1);
-        });
-    }
+  methods: {
+    // handleVote() {
+    //   this.votedPost ? this.onNoVote(this.location.id) : this.onUpVote(this.location.id);
+    // }
+    handleRemove(id) {
+      if(confirm('Are you sure you want to delete?')) {
+        return removeLocation(id)
+          .then(()=> {
+            const index = this.location.findIndex(tip => tip.id === id);
+            if(index === -1) return;
+            this.location.splice(index, 1);
+          });
+      }
+    },
+  
   },
+
 };
 </script>
 
