efa5fb350c9b20895457f0076e120b8f383815a4
Arthur Jen <arthurljen@gmail.com>
2018-06-20 14:51:09 -0700

    advice update complete

--- src/components/Tip.vue
+++ src/components/Tip.vue
@@ -1,20 +1,34 @@
 <template>
   <li>
-    <button
-      :class="{ upvoted: votedPost }"
-      @click="handleVote">⬆️</button>
-    <h3>( {{ tip.upvotes }} ) {{ tip.title }}</h3>
-    <p>{{ tip.text }}</p>
-    <h6>Submitted by {{ tip.firstName }} {{ tip.lastName }}</h6>
-    <button>⭐</button>
-    <button v-if="user.id === tip.authorID" @click="onRemove(tip.id)">❌</button>
-    <button v-if="user.id === tip.authorID">✏️</button>
+    <div v-if="!updating">
+      <button
+        :class="{ upvoted: votedPost }"
+        @click="handleVote">⬆️</button>
+      <h3>( {{ tip.upvotes }} ) {{ tip.title }}</h3>
+      <p>{{ tip.text }}</p>
+      <h6>Submitted by {{ tip.firstName }} {{ tip.lastName }}</h6>
+      <button>⭐</button>
+      <button v-if="user.id === tip.authorID" @click="onRemove(tip.id)">❌</button>
+      <button v-if="user.id === tip.authorID" @click="updating = true">✏️</button>
+    </div>
+    <AdviceForm
+      v-if="updating"
+      :onCancel="handleCancel"
+      :onEdit="handleUpdate"
+      :tip="tip"
+      />
   </li>
 </template>
 
 <script>
+import AdviceForm from './AdviceForm';
 export default {
-  props: ['tip', 'user', 'onRemove', 'votes', 'onUpVote', 'onNoVote'],
+  data() {
+    return {
+      updating: false
+    };
+  },
+  props: ['tip', 'user', 'onRemove', 'votes', 'onUpVote', 'onNoVote', 'onUpdate'],
   computed: {
     votedPost() {
       if(this.votes) {
@@ -26,9 +40,20 @@ export default {
   methods: {
     handleVote() {
       this.votedPost ? this.onNoVote(this.tip.id) : this.onUpVote(this.tip.id);
+    },
+    handleCancel() {
+      this.updating = false;
+    },
+    handleUpdate(toUpdate) {
+      return this.onUpdate(toUpdate)
+        .then(() => {
+          this.updating = false;
+        });
     }
+  },
+  components: {
+    AdviceForm
   }
-  
 };
 
 
